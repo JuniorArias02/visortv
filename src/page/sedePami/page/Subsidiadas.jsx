f5ccbd629eece8af7fe3d218d2c9185052b7e53c
JuniorArias02 <junior.arias02yt@gmail.com>
2026-01-06 10:41:56 -0500

    usando iframe 2

--- src/page/sedePami/page/Subsidiadas.jsx
+++ src/page/sedePami/page/Subsidiadas.jsx
@@ -4,7 +4,7 @@ function Subsidiadas() {
 	return (
 		<div className="flex items-center justify-center h-screen bg-white overflow-hidden">
 			<iframe
-				src="https://docs.google.com/presentation/d/e/2PACX-1vSzUPZ1LhpEZwmrOjOAUHwpJeK7WqOs5ei45YVV4s0y1N-bb-suvjeEIvS27TDTnuMgc4vcNQmIpQJV/pubembed?start=true&loop=false&delayms=30000"
+				src="https://docs.google.com/presentation/d/e/2PACX-1vSzUPZ1LhpEZwmrOjOAUHwpJeK7WqOs5ei45YVV4s0y1N-bb-suvjeEIvS27TDTnuMgc4vcNQmIpQJV/pub?start=true&loop=true&delayms=30000"
 				frameBorder="0"
 				width="1440"
 				height="839"
